5f2692a768a184efac80b2c60c3d515b8f099103
Andrew Wade <andrew.richard.wade@gmail.com>
2018-06-12 13:55:58 -0700

    (Untested) Stripping material from VacCan env original and adding EPICS

--- gym/envs/epics_slow_control/EPICSInterface.py
+++ gym/envs/epics_slow_control/EPICSInterface.py
@@ -3,34 +3,63 @@ from gym import logger
 import gym.spaces as spaces
 from gym.utils import seeding
 import numpy as np
-from scipy.integrate import odeint
+# from scipy.integrate import odeint
+from time import sleep
+
+# Channel access
+from ezca import Ezca
 
 
 class EPICSInterfaceEnv(gym.Env):
+    ''' This environment interfaces real world slow EPICS channels into the
+        standard form of a gym environment. Real training can then take place
+        using real world systems.
+
+        This environment is still beta and care should be taken that any
+        possible actions of the environement in the real world are safe.
+
+        In this initial implementation we are interfacing with laser slow
+        controls of the Coating Thermal Noise experiment.  Channels are
+        hard coded for now.  The goal is to achieve fastest possible lock
+        of pair of cavities.  Avaliable actions are binary lock switch and
+        laser slow control voltage.  Observations will be the reflected and
+        transmitted power of cavity.  Eventually we may also include the task
+        of holding the slow laser frequency close to a set point.
+
+        Its note clear if we can use continuous variables for the action state
+        atm.  For now we will implment steps as actions to increment epics
+        up and down.  As a first pass this will be up/down/stay put.  But
+        the next step would be to implement a range of values for size fo step.
+        '''
+
     metadata = {
         'render.modes':['human']
     }
 
     def __init__(self):
-        self.k = 1.136*25e-3
-        self.m = 15.76
-        self.C = 505
-        self.A = 1.3
+        RCPID = Ezca(ifo=None, logger=False)  # ezca python access to EPICS
         self.d = 5.08e-2
         self.t_step = 0.1  # seconds between state updates
         self.t_max = 10  # 10 seconds = 1 time-step
 
-        # Set-point temperature
-        self.T_setpoint = 45  # Celsius
+        # Set-point of process
+        self.T_setpoint = 1.0  # volts
 
-        # Temperature at which to fail the episode
-        self.T_threshold = 60
+        # Set bounds on search range hit the edge and you fail episode
+        self.ActuatorUB = 3.8
+        self.ActuatorLB = 3.1
 
-        self.action_space = spaces.Discrete(20)
+        # Observation chan one win case threshold
+        self.TransLockThresh = 2.0
+        self.RelfLockThresh = 0.8
+        self.FSSFastUB = 1.2
+        self.FSSFastLB = 0.8
+
+        # Establish action space
+        self.action_space = spaces.Discrete(6)
         self.observation_space = spaces.Box(np.array([15.0, 0.0]),
                                             np.array([60.0, 50.0]),
                                             dtype=np.float64)
-
         self.seed()
         # self.state = None
         self.steps_beyond_done = None
@@ -43,22 +72,6 @@ class EPICSInterfaceEnv(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
-# Physical Model of Vacuum Can temperature
-    def vac_can(self, T, t_inst):
-        # dTdt = -self.k*self.A*(T-self.T__env_buff[np.argmax(self.t >=\
-        #        t_inst)])/(self.d*self.m*self.C) \
-        #       + self.H_buff[np.argmax(self.t>= t_inst)]/(self.m*self.C)
-
-        dTdt = -self.k*self.A*(T-self.T_amb(t_inst))/(self.d*self.m*self.C) \
-               + self.P_heat/(self.m*self.C)
-        return dTdt
-
-# Ambient temperature function/list
-    def T_amb(self, time):
-        """Returns ambient temperature oscillating around 20 C with an
-           amplitude of 5 C, depending on number of steps elapsed. """
-        return 5*np.sin(2*np.pi*(self.elapsed_steps*10. + time)/(6*3600)) + 20.
-
 
 # Simulates reaction
     def step(self, action):
@@ -66,31 +79,22 @@ class EPICSInterfaceEnv(gym.Env):
                 "%r (%s) invalid" % (action, type(action))
 
         self.elapsed_steps += 1
-
-        T_can = self.state[0]
+        ProcessChan = "C3:PSL-SCAV_TRANS_DC"
+        ProcessVal = RCPID.read(ProcessChan)
 
         self.P_heat = action
 
-        self.t = np.arange(0, self.t_max, self.t_step)
-
-        #  self.T__env_buff = np.interp(self.t, self.t, T_amb)
-        #  self.H_buff = np.interp(self.t, self.t, P_heat)
-
         #  gets final value after integration
-        T_can_updated = float(odeint(
-            self.vac_can, T_can, self.t)[int(self.t_max/self.t_step) - 1])
 
-        self.state = np.array([T_can_updated,
-                               self.T_amb(self.elapsed_steps*10.)])
-
-        done = T_can_updated < 15 or T_can_updated > 60
+        self.state = ProcessVal
+        sleep(2)
+        done = ProcessVal > 1.5
         done = bool(done)
 
         if not done:
-            if self.state[0] > 40 or self.state[0] < 50:
+            if self.state[0] > 1.5 or self.state[0] < 4.0:
                 reward = 1.0
         elif self.steps_beyond_done is None:
-
             self.steps_beyond_done = 0
             reward = 1.0
         else:
@@ -106,6 +110,7 @@ class EPICSInterfaceEnv(gym.Env):
         return self.state, reward, done, {}
 
     def reset(self):
-        self.state = [self.np_random.uniform(low=15, high=30), self.T_amb(0)]
+        #self.state = [self.np_random.uniform(low=15, high=30), self.T_amb(0)]
+        self.state = 1.0  # dummy state to start
         self.steps_beyond_done = None
         return np.array(self.state)
